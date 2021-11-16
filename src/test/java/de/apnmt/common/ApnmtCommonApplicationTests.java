61e2db7f0a6c858395f8897d7ced27fef747c310
Tobias Wittmann <tobias.wittmann96@web.de>
2021-11-16 14:48:36 +0100

    initial commit

---
+++ src/test/java/de/apnmt/common/ApnmtCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package de.apnmt.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnmtCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
