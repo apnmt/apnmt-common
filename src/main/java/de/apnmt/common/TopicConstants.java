481d8a144817c08f04896ede3332daca66aa9cc6
tobi5775 <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>
2021-12-06 09:45:15 +0100

    extract errors into common

--- src/main/java/de/apnmt/common/TopicConstants.java
+++ src/main/java/de/apnmt/common/TopicConstants.java
@@ -6,10 +6,10 @@ public class TopicConstants {
 
     public static final String SERVICE_CHANGED_TOPIC = "service.changed";
 
-    public static final String CLOSING_TIME_CHANGED_TOPIC = "";
+    public static final String CLOSING_TIME_CHANGED_TOPIC = "closing.time.changed";
 
-    public static final String OPENING_HOUR_CHANGED_TOPIC = "";
+    public static final String OPENING_HOUR_CHANGED_TOPIC = "opening.hour.changed";
 
-    public static final String WORKING_HOUR_CHANGED_TOPIC = "";
+    public static final String WORKING_HOUR_CHANGED_TOPIC = "working.hour.changed";
 
 }
