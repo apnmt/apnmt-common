25c101322b497e55881e943550dcc12d8a92dd0a
TWI <audi-connect@msg.group>
2022-01-11 15:15:12 +0100

    rename topics due to aws sns doesn't allow points

--- src/main/java/de/apnmt/common/TopicConstants.java
+++ src/main/java/de/apnmt/common/TopicConstants.java
@@ -2,16 +2,16 @@ package de.apnmt.common;
 
 public class TopicConstants {
 
-    public static final String APPOINTMENT_CHANGED_TOPIC = "appointment.changed";
+    public static final String APPOINTMENT_CHANGED_TOPIC = "appointment-changed";
 
-    public static final String SERVICE_CHANGED_TOPIC = "service.changed";
+    public static final String SERVICE_CHANGED_TOPIC = "service-changed";
 
-    public static final String CLOSING_TIME_CHANGED_TOPIC = "closing.time.changed";
+    public static final String CLOSING_TIME_CHANGED_TOPIC = "closing-time-changed";
 
-    public static final String OPENING_HOUR_CHANGED_TOPIC = "opening.hour.changed";
+    public static final String OPENING_HOUR_CHANGED_TOPIC = "opening-hour-changed";
 
-    public static final String WORKING_HOUR_CHANGED_TOPIC = "working.hour.changed";
+    public static final String WORKING_HOUR_CHANGED_TOPIC = "working-hour-changed";
 
-    public static final String ORGANIZATION_ACTIVATION_CHANGED_TOPIC = "organization.activation.changed";
+    public static final String ORGANIZATION_ACTIVATION_CHANGED_TOPIC = "organization-activation-changed";
 
 }
